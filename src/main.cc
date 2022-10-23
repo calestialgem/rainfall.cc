5754e8d2c7066bd4a4d3865dd18652edf238bee6
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-23 10:33:11 +0300

    clean: Project

--- src/main.cc
+++ src/main.cc
@@ -1,40 +1,4 @@
 // SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "linear.cc"
-#include "tester.cc"
-
-/* Run the unit tests. Returns whether all tests passed. */
-bool RunTests() {
-  Tester tester;
-  Tester::RegisterTests(tester);
-  Linear::RegisterTests(tester);
-  return tester.Run();
-}
-
-/* Entry to the compiler. */
-int main(int argumentCount, char const* const* arguments) {
-  // Run the unit tests.
-  if (!RunTests()) return -1;
-
-  // Load the current working directory if no arguments are given.
-  char const* workspaceDirectory = ".";
-  if (argumentCount == 2) {
-    workspaceDirectory = std::span(arguments, argumentCount)[1];
-  } else if (argumentCount > 2) {
-    std::cerr << "Usage: rainfall [workspace-directory](default: .)"
-              << std::endl;
-    return -1;
-  }
-
-  auto workspace = Linear::LoadWorkspace(workspaceDirectory);
-
-  if (workspace.packages.empty()) {
-    std::cerr << "There are no packages in the workspace!" << std::endl;
-    return -1;
-  }
-
-  std::cout << "Loaded packages: " << std::endl;
-  for (auto const& [name, package] : workspace.packages)
-    std::cout << std::format("- {}", name) << std::endl;
-}
+int main(int argc, char const* const* argv) {}
---
+++ src/view.cc
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <algorithm>
+#include <cstddef>
+
+/* Immutable view of a linear array of elements. */
+template<typename Element>
+class View {
+  /* Pointer to the first element. */
+  Element const* first;
+  /* Pointer after the last element. */
+  Element const* after;
+
+public:
+
+  /* View to nowhere, without elements. */
+  static constexpr View EMPTY = {NULL, NULL};
+
+  /* View of a null-terminated array. */
+  [[nodiscard]] static constexpr View
+  ofTerminated(Element const* terminated) noexcept {
+    Element const* end = terminated;
+    while (*end) end++;
+    return {terminated, end};
+  }
+
+  /* Whether the given view equals to this one. */
+  [[nodiscard]] bool constexpr operator==(View const& other) const noexcept {
+    return std::ranges::equal(this, other);
+  }
+
+  /* Amount of elements in the view. */
+  [[nodiscard]] constexpr std::size_t size() const noexcept {
+    return after - first;
+  }
+
+  /* Iterator to the begining of the view. */
+  [[nodiscard]] constexpr Element const* begin() const noexcept {
+    return first;
+  }
+
+  /* Iterator to the end of the view. */
+  [[nodiscard]] constexpr Element const* end() const noexcept { return after; }
+
+  /* Immutable iterator to the begining of the view. */
+  [[nodiscard]] constexpr Element const* cbegin() const noexcept {
+    return first;
+  }
+
+  /* Immutable iterator to the end of the view. */
+  [[nodiscard]] constexpr Element const* cend() const noexcept { return after; }
+};
