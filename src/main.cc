886e341ac795f7056a767bc974031363316b5f2d
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-23 10:42:16 +0300

    clean: Project Again

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,14 @@
 // SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-int main(int argc, char const* const* argv) {}
+#include <cstdio>
+
+/* Entry to the compiler. */
+int main(int argc, char const* const* argv) {
+  if (argc > 2) {
+    std::fprintf(stderr, "Usage: rainfall [workspace-directory](default:.)\n");
+    return -1;
+  }
+  char const* workspaceDirectory = argc == 2 ? argv[1] : ".";
+  std::printf("Workspace: %s\n", workspaceDirectory);
+}
