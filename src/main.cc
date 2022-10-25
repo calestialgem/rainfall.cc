ce25c4ead79d2d8b5d047ff16a1d389cb63b21ec
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-25 17:31:50 +0300

    clean: Project

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,9 @@
 // SPDX-FileCopyrightText: 2022 Cem Geçgel <gecgelcem@outlook.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "Printer.cc"
+#include <iostream>
 
 /* Entry to the compiler. */
 int main(int argc, char const* const* argv) {
-  if (argc > 2) {
-    Print("Usage: rainfall [workspace-directory](default:.)");
-    return -1;
-  }
-  char const* workspaceDirectory = argc == 2 ? argv[1] : ".";
-  Print("Workspace: %", workspaceDirectory);
+  std::cout << "Hello!" << std::endl;
 }
